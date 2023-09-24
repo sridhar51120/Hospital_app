061f876a46c61dd844107880341534ae67f48e63
SRIDHAR DSCV <sridhardscv@gmail.com>
2023-09-24 23:41:17 +0530

    find docter

--- app/src/main/java/com/example/hospital_app/HomeActivity.java
+++ app/src/main/java/com/example/hospital_app/HomeActivity.java
@@ -1,8 +1,14 @@
 package com.example.hospital_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username","").toString();
+        Toast.makeText(getApplicationContext(),"welcome "+username, Toast.LENGTH_SHORT).show();
+
+        CardView exit = findViewById(R.id.cardExit);
+        exit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            }
+        });
+        CardView findDocter = findViewById(R.id.cardFindDocter);
+        findDocter.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(HomeActivity.this, FindDocterActivity.class));
+            }
+        });
+
     }
 }
