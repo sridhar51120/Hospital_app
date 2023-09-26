63e134abcd7c4c2d0e654fc8b775656f260fc625
SRIDHAR DSCV <sridhardscv@gmail.com>
2023-09-26 14:45:28 +0530

    find docter template

--- app/src/main/java/com/example/hospital_app/FindDocterActivity.java
+++ app/src/main/java/com/example/hospital_app/FindDocterActivity.java
@@ -1,8 +1,11 @@
 package com.example.hospital_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FindDocterActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class FindDocterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_docter);
+
+        CardView exit = findViewById(R.id.cardFDBack);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FindDocterActivity.this,HomeActivity.class
+                ));
+            }
+        });
     }
 }
